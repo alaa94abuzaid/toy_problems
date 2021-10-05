c9eb0b444391b816bb712d5c991c3607ecc76ffa
alaa-abuzaid <aabuzaid@voice.com>
2021-10-05 11:56:04 +0300

    custom function

--- addingCustomFunctions/index.test.js
+++ addingCustomFunctions/index.test.js
@@ -7,3 +7,45 @@
  */
 
 //  TODO
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.sortArray = function() {
+  return this.sort();
+};
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.getFirstElement = function() {
+  return this[0];
+};
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.getLastElement = function() {
+  return this[this.length - 1];
+};
+
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.findElenment = function(element) {
+  return this.indexOf(element);
+};
+
+describe('Test Custom functions', () => {
+  it('Test getFirstElement', () => {
+    expect([1, 2, 3].getFirstElement()).toEqual(1);
+  });
+
+  it('Test getLastElement', () => {
+    expect([1, 2, 3].getLastElement()).toEqual(3);
+    expect([5, 2, 3].sortArray()).toEqual([2, 3, 5]);
+    expect([5, 2, 3].findElenment(2)).toEqual(1);
+  });
+
+  it('Test sortArray', () => {
+    expect([5, 2, 3].sortArray()).toEqual([2, 3, 5]);
+    expect([5, 2, 3].findElenment(2)).toEqual(1);
+  });
+
+  it('Test findElenment', () => {
+    expect([5, 2, 3].findElenment(2)).toEqual(1);
+  });
+});
