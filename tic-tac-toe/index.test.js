54d75218ca3f01eba9e65104d58c0b9736fe0906
alaa-abuzaid <aabuzaid@voice.com>
2021-10-10 12:08:43 +0300

    ticTacToe

--- tic-tac-toe/index.test.js
+++ tic-tac-toe/index.test.js
@@ -23,3 +23,84 @@ var solvedBoard = [[0,1,1],
                    [2,1,2]];
 ticTacToe(solvedBoard);//should return 1
 */
+
+const ticTacToe = (arr) => {
+  const checkRows = () => {
+    let row = [];
+    let winner;
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+        row = arr[i];
+        if (row[j] !== row[j + 1]) {
+          break;
+        } else {
+          winner = row[j];
+        }
+      }
+    }
+    return winner;
+  };
+
+  const checkColumns = () => {
+    let column = [];
+    let winner;
+    for (i = 0; i < 2; i++) {
+      for (j = 0; j < 2; j++) {
+        column = [arr[0][i], arr[1][i], arr[2][i]];
+        if (column[j] !== column[j + 1]) {
+          break;
+        } else {
+          winner = column[j];
+        }
+      }
+    }
+    return winner;
+  };
+
+  //   const checkDiag = () => {
+  //     const diags = [
+  //       [arr[0][0], arr[1][1], arr[2][2]],
+  //       [arr[0][2], arr[1][1], arr[2][0]],
+  //     ];
+  //     console.log('diags', diags);
+  //     let winner;
+  //     for (i = 0; i < 2; i++) {
+  //       for (j = 0; j < 2; j++) {
+  //         if (diags[i][j] !== diags[i][j + 1]) {
+  //           break;
+  //         } else {
+  //           winner = diags[i][j];
+  //         }
+  //       }
+  //     }
+  //     return winner;
+  //   };
+
+  rows = checkRows();
+  columns = checkColumns();
+  if (rows) {
+    return rows;
+  } else if (columns) {
+    return columns;
+  } else {
+    return -1;
+  }
+};
+
+describe('Tests', () => {
+  it('test solveTic', () => {
+    const board = [
+      [0, 0, 1],
+      [0, 1, 2],
+      [2, 1, 0],
+    ];
+    const solvedBoard = [
+      [0, 1, 1],
+      [0, 1, 2],
+      [2, 1, 2],
+    ];
+
+    expect(ticTacToe(board)).toEqual(-1);
+    expect(ticTacToe(solvedBoard)).toEqual(1);
+  });
+});
