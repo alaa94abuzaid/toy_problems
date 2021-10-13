7313d8efa861806a24e409fdcc6dd3604efbdb6e
alaa-abuzaid <aabuzaid@voice.com>
2021-10-13 15:23:57 +0300

    nextBigger

--- nextBigger/index.test.js
+++ nextBigger/index.test.js
@@ -14,5 +14,51 @@ nextBigger(531)==-1
 */
 
 const nextBigger = function(num) {
+  const d = num.toString().split('');
 
+  let p = -1;
+  for (i = d.length-1; i > 0; i--) {
+    if (d[i] > d[i-1]) {
+      p = i-1;
+      break;
+    }
+  }
+
+  if (p == -1) return p;
+
+  let right = d.splice(p);
+
+  const pv = right.splice(0, 1)[0];
+
+  let mm = null; let mmi = null;
+  for (i = 0; i < right.length; i++) {
+    if (right[i] > pv) {
+      if (mm == null || right[i] < mm) {
+        mm = right[i];
+        mmi = i;
+      }
+    }
+  }
+
+  if (mmi == null) return -1;
+
+  right.splice(mmi, 1);
+  right.push(pv);
+  right = right.sort();
+
+  const ret = +d.concat([mm]).concat(right).join('');
+  if (ret < num) return -1;
+
+  return ret;
 };
+
+describe('Tests', () => {
+  it('test nextBigger', () => {
+    expect(nextBigger(12)).toEqual(21);
+    expect(nextBigger(513)).toEqual(531);
+    expect(nextBigger(2017)).toEqual(2071);
+    expect(nextBigger(9)).toEqual(-1);
+    expect(nextBigger(111)).toEqual(-1);
+    expect(nextBigger(531)).toEqual(-1);
+  });
+});
