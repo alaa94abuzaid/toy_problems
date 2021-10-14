5c5388b0ac017a47b6aa530387cff897656adaeb
alaa-abuzaid <aabuzaid@voice.com>
2021-10-14 12:03:42 +0300

    more than one max

--- secondMax/index.test.js
+++ secondMax/index.test.js
@@ -16,8 +16,14 @@ secondMax([-1, -5, 2])== -1
  * @return {Number} second maximum number in the array
  */
 const secondMax = function(array) {
+  const max = Math.max(...array);
   array.splice(array.indexOf(Math.max(...array)), 1);
-  return Math.max(...array);
+  let max2 = Math.max(...array);
+  while (max2 === max) {
+    array.splice(array.indexOf(max2), 1);
+    max2 = Math.max(...array);
+  }
+  return max2;
 };
 
 
@@ -25,5 +31,6 @@ describe('secondMax', () => {
   it('test secondMax', () => {
     expect(secondMax([1, 5, 2])).toEqual(2);
     expect(secondMax([-1, -5, 2])).toEqual(-1);
+    expect(secondMax([1, 2, 0, 5, 5, 5])).toEqual(2);
   });
 });
