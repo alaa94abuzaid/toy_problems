e825b3b159092faa026d025e475bb129f8150b1d
alaa-abuzaid <aabuzaid@voice.com>
2021-10-14 11:31:07 +0300

    secondMax

--- secondMax/index.test.js
+++ secondMax/index.test.js
@@ -10,6 +10,20 @@ secondMax([1,5, 2])== 2
 secondMax([-1, -5, 2])== -1
 */
 
+/**
+ * find second maximum number in an array of integer numbers
+ * @param {Array} array array of integer numbers
+ * @return {Number} second maximum number in the array
+ */
 const secondMax = function(array) {
-  // TODO
+  array.splice(array.indexOf(Math.max(...array)), 1);
+  return Math.max(...array);
 };
+
+
+describe('secondMax', () => {
+  it('test secondMax', () => {
+    expect(secondMax([1, 5, 2])).toEqual(2);
+    expect(secondMax([-1, -5, 2])).toEqual(-1);
+  });
+});
